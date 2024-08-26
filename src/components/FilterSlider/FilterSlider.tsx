5f30b24da1805a0814b93351bed2d54b1d63a4b9
Esly Lescano <eslylescano@gmail.com>
2024-08-26 20:04:14 +0100

    Add filter slider ui

--- src/components/FilterSlider/FilterSlider.tsx
+++ src/components/FilterSlider/FilterSlider.tsx
@@ -1,5 +1,16 @@
+import "./FilterSlider.css"
 export default function FilterSlider() {
   return (
-    <div>FilterSlider</div>
+    <div className="filter-slider">
+      <h3 className="filter-slider-title">Filters</h3>
+      <p className="filter-slider-description">Spending</p>
+      <div className="filter-slider-container">
+        <input type="range" min="1" max="100" value="50"></input>
+      </div>
+      <div className="filter-slider-range">
+        <p className="filter-slider-range-paragraph">$63950</p>
+        <p className="filter-slider-range-paragraph">$26191</p>
+      </div>
+    </div>
   )
 }
